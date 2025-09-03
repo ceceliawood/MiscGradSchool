fcae710b1ffbb2eac48729d36eac3777b979a329
ceceliawood <cecelia@vt.edu>
2025-09-03 10:28:23 -0400

    Added new variables to model

--- Beta Regression/BetaregExample.R
+++ Beta Regression/BetaregExample.R
@@ -43,6 +43,35 @@ frame1 <- read_csv('https://raw.githubusercontent.com/ceceliawood/MiscGradSchool
 
 plot(frame1)
 
+
+frame2 <- read_csv('https://raw.githubusercontent.com/ceceliawood/MiscGradSchool/refs/heads/main/Beta%20Regression/Data%20Mining_all.csv') %>% 
+  mutate(Gt_pct = Gt_pct/100, # turn percents into decimals
+         Lp_pct = Lp_pct/100,
+         Fh_pct = Fh_pct/100,
+         Mt_pct = Mt_pct/100) %>% 
+  rename('EV1' = 'Buffer1',        # renaming variables for ease of use
+         'EV2' = 'CounterIon1',    # EV = explanatory variable
+         'EV3' = 'pH',             # RV = response variable
+         'EV4' = 'Fe2Fh_mMgL',
+         'EV5' = 'Time_hr',        #note that in Buffer1, 'A' = unbuffered
+         'EV6' = 'CFe_molmol',
+         'EV7' = 'Al_molpercent',
+         'EV8' = 'SiFe_molmol',
+         'RV1' = 'Fh_pct',
+         'RV2' = 'Gt_pct',
+         'RV3' = 'Lp_pct',
+         'RV4' = 'Mt_pct') %>%
+  select(contains('V')) %>% 
+  #slight transformations to meet assumptions of beta reg
+  mutate(across(contains('RV'), ~if_else(. == 0, 0.0001, .))) %>% 
+  mutate(across(contains('RV'), ~if_else(. == 1, 0.9999, .))) %>%
+  mutate(EV5 = if_else(EV5 == 0, 0.001, EV5)) %>%
+  mutate(EV6 = if_else(EV6 == 0, 0.001, EV6)) %>% 
+  mutate(EV7 = if_else(EV7 == 0, 0.001, EV7)) %>% 
+  mutate(EV8 = if_else(EV8 == 0, 0.001, EV8))
+
+plot(frame2)
+
 frame1_filt <- frame1 %>% 
   filter(EV5 < 2000) #testing getting rid of highest time point
 
@@ -57,17 +86,37 @@ frame2 <- frame1 %>%
 # If RV1 = 0.75, then 75% of the mixed sample is phase 1
 # Thus, RVs range from 0-1 and include both 0 and 1
 
-RV2_logit <- betareg(RV2 ~ EV1 + EV2 + log(EV4) + log(EV5) + log(EV6) + log(EV7) + EV8 | EV5, data = frame1,
+RV2_logit <- betareg(RV2 ~ EV1 + EV2 + EV3 + log(EV4) + log(EV5) + log(EV6) + log(EV7) + EV8 | EV5, data = frame1,
                     link = 'logit')
 summary(RV2_logit)
 plot(RV2_logit)
 
 
 
-RV2_logit2 <- betareg(RV2 ~ EV1 + EV2 + log(EV4) + log(EV5) + log(EV6) + log(EV7) + EV8 | log(EV5), data = frame1,
+RV2_logit2 <- betareg(RV2 ~ EV1 + EV2 + EV3 + log(EV4) + log(EV5) + log(EV6) + log(EV7) + EV8 | log(EV5), data = frame1,
                      link = 'logit')
 summary(RV2_logit2)
 plot(RV2_logit2)
+
+lrtest(RV2_logit2, RV2_logit)
+
+############
+
+RV2_logit2b <- betareg(RV2 ~ EV1 + EV2 + log(EV5) + log(EV6) + EV8 | EV5, data = frame2,
+                      link = 'logit')
+summary(RV2_logit2b)
+plot(RV2_logit2b)
+
+
+RV2_logit2b2 <- betareg(RV2 ~ EV1 + EV2 + EV4 + log(EV5) + log(EV6)+ EV8 | EV5, data = frame2,
+                       link = 'logit')
+summary(RV2_logit2b2)
+
+lrtest(RV2_logit2b, RV2_logit2b2)
+
+
+
+
 
 
 RV2_logit_table2 <- RV2_logit2 %>% 
@@ -79,7 +128,7 @@ RV2_logit_table2 <- RV2_logit2 %>%
 
   
        
-lrtest(RV2_logit2, RV2_logit)
+
 
 
 
@@ -102,3 +151,6 @@ RV4_logit1 <- betareg(RV4 ~ EV1 + EV2 + EV3 + EV4 + EV5 + EV6 + EV7 + EV8 + EV4*
 summary(RV4_logit1)
 plot(RV4_logit1)
 
+
+test <- lm(frame2$RV4 ~ sqrt(frame2$EV4))
+summary(test)
